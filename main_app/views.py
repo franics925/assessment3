a2f7e7901d8829359e0c3757618488df991fa1dc
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-09-21 01:33:55 -0700

    update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
+from .models import Widget
+from .forms import WidgetForm
 
 # Create your views here.
 
 
 def home(request):
-  return render(request, 'home.html')
+    widget_list = Widget.objects.all()
+    form = WidgetForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    else:
+        form = WidgetForm()
+    return render(request, 'home.html', {'form': form, 'widget_list': widget_list})
