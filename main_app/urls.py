eb00c712b665a56db2f6f82698c1246b4ac2cccc
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-09-21 00:07:48 -0700

    create format for html page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,3 @@ urlpatterns = [
   path('', views.home, name='home'),
 #   path('widget/<int:widget_id>/', views.delete, name='delete')
 ]
-
-
-def home(request):
-  return render(request, 'home.html')
